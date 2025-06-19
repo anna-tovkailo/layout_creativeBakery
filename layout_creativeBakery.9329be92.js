65e68ec18ba99e558e682e1013fe7b78b3723a78
anna-tovkailo <babylike24@gmail.com>
2025-06-19 14:35:58 +0300

    make build

---
+++ layout_creativeBakery.9329be92.js
@@ -0,0 +1,2 @@
+"use strict";window.addEventListener("hashchange",()=>{"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});
+//# sourceMappingURL=layout_creativeBakery.9329be92.js.map
